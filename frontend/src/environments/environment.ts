677ea160115d9cdcff0bbfc1a312e9ee87a8e37f
Nahuel Monje <102997844+monjenahuel@users.noreply.github.com>
2024-09-24 21:57:32 -0300

    test

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api',
+    // apiUrl: 'http://localhost:3000/api',
+    apiUrl: 'http://144.22.157.204/api',
 };
